d56cea785cd6b6a9c682b8a355af5f9a3bbf2caa
Lucas Mariani <lmariani@icloud.com>
2025-06-14 15:13:02 +0200

    Initial Commit

---
+++ VocabularyBuilder/VocabularyBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyBuilderApp.swift
+//  VocabularyBuilder
+//
+//  Created by Lucas on 14.06.25.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
